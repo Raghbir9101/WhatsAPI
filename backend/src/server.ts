8a71aa686fee10badbcb28a5b9bc38a96989e2ed
Raghbir Singh <rkgaming0203@gmail.com>
2025-07-14 16:10:30 +0530

    build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,7 +44,7 @@ connectDB().then(() => {
 });
 
 // Basic middleware
-app.use(helmet());
+// app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
